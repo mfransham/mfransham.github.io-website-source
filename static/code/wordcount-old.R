34b98d5f278045818a62235cdb2f8ba7f9aed76d
Mark Fransham <Mark.Fransham@apsoc.ox.ac.uk>
2025-01-10 09:56:40 +0000

    updated wordcount function

--- static/code/wordcount-old.R
+++ static/code/wordcount-old.R
@@ -1,16 +1,12 @@
 # function for calculating number of words in R Markdown document
-# regular expressions in sub / gsub removes all YAML, code chunks, inline code
-# also removes # symbols used to create section headers 
+# regular expression in gsub removes all YAML, code chunks and inline code
 # requires the magrittr package for the pipe operator
 wordcount <- function(rmd) {
   if ( !("magrittr" %in% installed.packages() ) ) {stop("Please install magrittr package") }
   library(magrittr)
   readChar(rmd, 
            nchars = file.info(rmd)$size) %>% 
-    sub("---.*?---", "", x = .) %>% 
-    gsub("```.*?```", "", x = .) %>% 
-    gsub("`r.*?`", "", x = .) %>% 
-    gsub("#", "", x = .) %>% 
+    gsub("---.*?---|```.*?```|`r.*?`|##", "", x = .) %>% 
     strsplit( "[[:space:]]+") %>% 
     unlist() %>% 
     length()
